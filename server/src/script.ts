cf197ac183883915b12fd36e2e4363dea67ee679
Benluv <luongov13@gmail.com>
2023-09-27 16:43:48 +0200

    added GraphQLContext for prisma client in order to enable communication with the db and prisma

--- server/src/script.ts
+++ server/src/script.ts
@@ -3,13 +3,13 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    const allLinks = await prisma.link.findMany()
     const newLink = await prisma.link.create({
         data: {
             description: 'Fullstack tutorial for GraphQL',
             url: 'www.howtographql.com'
         }
     })
+    const allLinks = await prisma.link.findMany()
     console.log(allLinks)
 }
 
